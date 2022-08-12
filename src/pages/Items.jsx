72e36bebac35e68d94b471c9db9cc47b98708ad8
Beka <bekzot.abdyldayev@gmail.com>
2022-08-13 00:16:27 +0600

    ui for sorts and filters

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -10,11 +10,14 @@ import { Pagination } from '../components/UI/Pagination/Pagination';
 import { Loader } from '../components/UI/Loader/Loader';
 import { Header } from '../components/UI/Header/Header';
 import { ContactForm } from '../components/UI/ContactForm/ContactForm';
+import { Button } from '../components/UI/Button/Button';
 export const Items = () => {
   const [items, setItems] = useState([]);
   const [filteredItems, setFilteredItems] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [isFilterOpenned, setIsFilterOpenned] = useState(false);
+  const [isSortOpenned, setIsSortOpenned] = useState(true);
 
   const [isFiltered, setIsFiltered] = useState(false);
 
@@ -98,12 +101,157 @@ export const Items = () => {
       <div className='wrapper'>
         <div className='options'>
           <div className='filters'>
-            <div className='filters__nav'>
+            <div
+              className='filters__nav'
+              onClick={() => {
+                setIsFilterOpenned(!isFilterOpenned);
+              }}
+            >
               <ArrowRight />
               <div className='options__text'>Фильтры</div>
             </div>
-            <div className='filters__items'>
-              {tags.map((tag) => (
+            {isFilterOpenned && (
+              <div className='filters__items'>
+                <div
+                  className='filters__nav filters__nav__inside'
+                  onClick={() => {
+                    setIsFilterOpenned(!isFilterOpenned);
+                  }}
+                >
+                  <div className='filters__arrow open'>
+                    <ArrowRight />
+                  </div>
+                  <div className='filters__title'>Фильтры</div>
+                </div>
+                <div className='filters__item'>
+                  <div className='filters__title'>
+                    <div className='filters__arrow open'>
+                      <ArrowRight />
+                    </div>
+                    Бренды
+                  </div>
+                  <div className='filter__open filters__subtitles'>
+                    <div className='filters__subtitle'>
+                      <input
+                        type='checkbox'
+                        name=''
+                        id=''
+                        className='filters__checkbox'
+                      />
+                      Bershka
+                    </div>
+                    <div className='filters__subtitle'>
+                      <input
+                        type='checkbox'
+                        name=''
+                        id=''
+                        className='filters__checkbox'
+                      />
+                      Pul&Bear
+                    </div>
+                    <div className='filters__subtitle'>
+                      <input
+                        type='checkbox'
+                        name=''
+                        id=''
+                        className='filters__checkbox'
+                      />
+                      Zara
+                    </div>
+                    <div className='filters__subtitle'>
+                      <input
+                        type='checkbox'
+                        name=''
+                        id=''
+                        className='filters__checkbox'
+                      />
+                      Tommy Hilfiger
+                    </div>
+                  </div>
+                </div>
+
+                <div className='filters__item'>
+                  <div className='filters__title'>
+                    <div className='filters__arrow open'>
+                      <ArrowRight />
+                    </div>
+                    Размер
+                  </div>
+                  <div className='filters__subtitles filter__open'>
+                    <div className='filters__subtitle'>
+                      <input
+                        type='checkbox'
+                        name=''
+                        id=''
+                        className='filters__checkbox'
+                      />
+                      S
+                    </div>
+                    <div className='filters__subtitle'>
+                      <input
+                        type='checkbox'
+                        name=''
+                        id=''
+                        className='filters__checkbox'
+                      />
+                      M
+                    </div>
+                    <div className='filters__subtitle'>
+                      <input
+                        type='checkbox'
+                        name=''
+                        id=''
+                        className='filters__checkbox'
+                      />
+                      L
+                    </div>
+                    <div className='filters__subtitle'>
+                      <input
+                        type='checkbox'
+                        name=''
+                        id=''
+                        className='filters__checkbox'
+                      />
+                      XL
+                    </div>
+                  </div>
+                </div>
+
+                <div className='filters__item'>
+                  <div className='filters__title'>
+                    <div className='filters__arrow open'>
+                      <ArrowRight />
+                    </div>
+                    Цена
+                  </div>
+                  <div className='filters__subtitles filter__open'>
+                    <div className='filters__subtitle'>
+                      От
+                      <input
+                        type='number'
+                        name=''
+                        id=''
+                        value={0}
+                        className='filters__price'
+                      />
+                      до
+                      <input
+                        type='number'
+                        name=''
+                        id=''
+                        value={0}
+                        className='filters__price'
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <Button
+                  text={'Применить фильтры'}
+                  style={{ width: '100%', height: 51 }}
+                />
+
+                {/* {tags.map((tag) => (
                 <div
                   className='filter__item'
                   onClick={() => addFilter({ tags: tag._id })}
@@ -111,14 +259,49 @@ export const Items = () => {
                 >
                   {tag.title}
                 </div>
-              ))}
-            </div>
+              ))} */}
+              </div>
+            )}
           </div>
-          <div className='sortings'>
-            <div className='options__text'>Сортировка</div>
+          <div
+            className='sortings'
+            onClick={() => {
+              setIsSortOpenned(!isSortOpenned);
+            }}
+          >
+            <div>Сортировка</div>
             <div>
               <ArrowRight />
             </div>
+            {isSortOpenned && (
+              <div className='sort__items'>
+                <div
+                  className='filters__nav filters__nav__inside'
+                  onClick={() => {
+                    setIsSortOpenned(!isSortOpenned);
+                  }}
+                >
+                  <div className='filters__arrow open'>
+                    <ArrowRight />
+                  </div>
+                  <div className='filters__title'>Сортировка</div>
+                </div>
+                <div className='sort__item'>
+                  <input type='radio' name='sort' id='asc' />
+                  <label htmlFor='asc'>Цена (по убыванию)</label>
+                </div>
+
+                <div className='sort__item'>
+                  <input type='radio' name='sort' id='desc' />
+                  <label htmlFor='desc'>Цена (по убыванию)</label>
+                </div>
+
+                <div className='sort__item'>
+                  <input type='radio' name='sort' id='date' />
+                  <label htmlFor='date'>Дата добавления</label>
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <div className='items__list'>
